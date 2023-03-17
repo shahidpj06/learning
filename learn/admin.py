9fc6e565d95e331ea1c161e02c0becfbb37d3c38
Shahid P J <127497564+shahidpj06@users.noreply.github.com>
2023-03-18 00:41:30 +0530

    Initial commit

---
+++ learn/admin.py
@@ -0,0 +1,13 @@
+from . models import *
+from django.contrib import admin
+
+admin.site.register(Registration)
+admin.site.register(Subject)
+admin.site.register(Enrollment)
+admin.site.register(Learning_progress)
+admin.site.register(Feedback)
+admin.site.register(Messages)
+admin.site.register(Exam)
+admin.site.register(Exam_results)
+admin.site.register(Blogs)
+admin.site.register(Requests)
